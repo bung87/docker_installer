7f470bb8a4bb44d748da4a5128f76a9ecc90d9f1
zhoupeng <zhoupeng@xjgreat.com>
2017-12-15 12:06:52 +0800

    o

--- setup.py
+++ setup.py
@@ -1,9 +1,31 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-import os
+import os,sys
+import pkg_resources
 
 version = '0.1.1'
+
+install_requires = [
+    'scp'
+]
+
+extras_require = {
+    ":python_version>='3'": ['beautifulsoup4'],
+    ":python_version<'3'": ['BeautifulSoup']
+}
+
+try:
+    if 'bdist_wheel' not in sys.argv:
+        for key, value in extras_require.items():
+            if key.startswith(':') and pkg_resources.evaluate_marker(key[1:]):
+                install_requires.extend(value)
+except Exception as e:
+    print("Failed to compute platform dependencies: {}. ".format(e) +   "All dependencies will be installed as a result.")
+    for key, value in extras_require.items():
+        if key.startswith(':'):
+            install_requires.extend(value)
+    extras_require = {}
 
 setup(
     name='docker_installer',
@@ -16,10 +38,8 @@ setup(
     url='https://github.com/bung87/docker_installer',
     packages=['docker_installer'],
     package_dir={'docker_installer': 'docker_installer'},
-    install_requires=[
-        'scp',
-        'BeautifulSoup'
-    ],
+    extras_require=extras_require,
+    install_requires=install_requires,
     entry_points={
         'console_scripts': [
             'docker_installer=docker_installer.the_installer:main'
